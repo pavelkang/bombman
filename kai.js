0027c765c94672c8959ff4e539ce200e04358610
kai <sclingtt@gmail.com>
2014-11-05 02:07:16 -0500

    des

---
+++ kai.js
@@ -0,0 +1,16 @@
+;(function() {
+    var w = 500;
+    var h = 500;
+    var canvas = document.getElementById("screen");
+    canvas.style.backgroundColor = "gray";
+
+    var $div = document.createElement("div");
+    $div.style.width = "100px";
+    $div.style.height = "100px";
+    $div.style.background = "red";
+
+    document.body.appendChild($div);
+
+    //setInterval(function(){alert("Hello")}, 3000);
+
+})();
